e0a1c1d2f41b8a8c5f0d8ef86eaea637eb49bbeb
MichValenz <michvalenz27@gmail.com>
2021-12-21 23:22:37 -0600

    added array of readme questions to ask user

--- develop/index.js
+++ develop/index.js
@@ -1,13 +1,69 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project/s title?'
+
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the project description?'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?'
+    },
+    {
+        type: 'input',
+        name: 'use',
+        message: 'Describe the use of the project/application.'
+    },
+    {
+        type: 'input',
+        name: 'contributions',
+        message: 'List those who made contributions to the project'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Provide the user instructions on how to run all necessary tests'
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'List frequently asked questions with answers'
+    },
+
+
+])
+
+  .then(answers => {
+    const filename = `${answers.name
+      .toLowerCase()
+      .split(' ')
+      .join('')}.json`;
+
+
+      //write README
+    fs.writeFile(filename, generateMarkdown(answers, null, '\t'), err =>
+      err ? console.log(err) : console.log('Success!')
+     );
+  });
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile("README.md", data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() {}
 
 // Function call to initialize app
-init();
+//init();
